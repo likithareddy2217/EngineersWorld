16108242814828e87c067571adf39df493dc24b3
SuryaNarendra <suryanarendra1000@gmail.com>
2024-11-07 10:23:55 +0530

    Add HomePage folder to components and create Home.js and Home.css in HomePage

--- src/components/navigationbar/navigation.js
+++ src/components/navigationbar/navigation.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import './navigation.css'
+import React, { useEffect, useState } from 'react';
+import "./Navigation.css";
 
-const NavigationComponent = () => {
+function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div className='navigation'>
-      <h1>Engineers world </h1>
-      <div className='links'>
-        <a href=""> Home</a>
-        <a href=""> About</a>
-        <a href=""> Experience</a>
-        <a href=""> Leadership</a>
-        <a href=""> Hire us</a>
+    <header className={isScrolled ? 'scrolled' : ''}>
+      <div className='logo-div'>
+        <img src="./images/logo.jpg" alt="Logo" height={40} width={40} />
+        <h1>ENGINEER'S WORLD</h1>
       </div>
-      <button className='contactBtn'>Contact Us</button>
-    </div>
+      <nav className='nav'>
+        <div className='links'>
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Experience</a>
+          <a href="#">Leadership</a>
+          <a href="#">Hire Us</a>
+        </div>
+        <button className='nav-btn'>Contact</button>
+      </nav>
+    </header>
   );
-};
+}
 
-export default NavigationComponent;
+export default Header;
