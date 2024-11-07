bdcbc946269dc165964bd21a8a581a7fc1a47dd7
Muli surya Narendra reddy <133997432+Narendra-017@users.noreply.github.com>
2024-11-07 18:54:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-<<<<<<< HEAD
+
 import "./App.css"
 import NavigationComponent from "./components/navigationbar/navigation"
-=======
+
 
 import NavigationComponent from "./components/navigationbar/navigation.js"
->>>>>>> 2d6720ad01166adda161dab147a40f3a70f6ddd7
 
 function App() {
   return (
     <div className="EngineersWorld">
      <NavigationComponent />
       {/* <p>hello </p> */}
-      <div className="background-img"></div>
     </div>
   );
 }
