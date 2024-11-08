27b88a73acb9c4e7407b0f2321886ad19af61535
SuryaNarendra <suryanarendra1000@gmail.com>
2024-11-08 15:02:13 +0530

    pull repository

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+// import { useEffect } from 'react';
 import "./App.css"
 import NavigationComponent from "./components/navigationbar/navigation";
 
 import AboutComponent from "./components/AboutPage/About";
 
 function App() {
+  
   return (
     <div className="EngineersWorld">
      <NavigationComponent />
