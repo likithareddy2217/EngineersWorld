8d142e9e418c82b8deea1696aa04c2f38682bdd8
SuryaNarendra <suryanarendra1000@gmail.com>
2024-11-10 22:59:22 +0530

    Apply changes

--- src/components/AboutPage/About.js
+++ src/components/AboutPage/About.js
@@ -8,13 +8,13 @@ function About() {
         <p>JOIN US IN OUR MISSION TO ENHANCE ONLINE SAFETY AND DRIVE TECHNOLOGICAL INNOVATION.</p>
       </div>
       <div className="about-heading">
-        <h1>ABOUT US <span className="full-stop"></span></h1>
+        <h1>ABOUT US.</h1>
       </div>
       <div className="about-content">
         <p>Engineers World, led by Mr Akshay Kumar, excels in creating user-friendly websites and innovative cybersecurity solutions. Our flagship product, PhishShield, uses advanced machine learning to protect against phishing attacks. We are dedicated to education, offering internships and online learning to nurture future tech leaders. Our team provides top-notch project and assignment solutions for universities and educational institutions, ensuring academic success.</p>
       </div>
 
-      <div className="content-div">
+      <div className="card-container">
         <div className="card">
           <img src="/images/webDevelopment.jpg" alt="" />
           <h2>Web</h2>
--- src/components/HirePage/Hire.js
+++ src/components/HirePage/Hire.js
@@ -5,22 +5,76 @@ function Hire(){
 
   return (
     <section className="hirepage">
+      <h1 className="hirepage-title">Empowering Academic Success and Online Safety Through Innovation</h1>
       <div className="hirepage-container">
-        <h2 className="hirepage-title">Empowering Academic Success and Online Safety Through Innovation</h2>
         <div className="hirepage-feature hirepage-feature-1">
-          
+          <h2>MISSION</h2>
+          <p>to give our best and give the best product</p>
+          <ul>
+            <li>
+              Our mission at Engineers World is to revolutionize the digital landscape by delivering innovative website development services.
+            </li>
+            <li>
+              We strive to empower our clients and learners with the tools and knowledge necessary to thrive in an interconnected world
+            </li>
+            <li>
+              Fostering the growth of emerging tech talent, and pioneering advanced AI-driven cybersecurity solutions
+            </li>
+            <li>
+              We strive to empower our clients and learners with the tools and knowledge necessary to thrive in an interconnected world
+            </li>
+          </ul>
         </div>
         <div className="hirepage-feature hirepage-feature-2">
-
+          <h2>VISION</h2>
+          <p>At Engineers World, our vision is to be the architects of a digital ecosystem</p>
+          <ul>
+            <li>
+              Where innovation, education, and security converge. We aim to set the standard for excellence in website development
+            </li>
+            <li>
+              Beacon for aspiring tech professionals through our comprehensive training,
+            </li>
+            <li>
+              Implementing responsive design and ensuring cross-browser compatibility.
+            </li>
+            <li>
+              Redefine cybersecurity with our cutting-edge AI and ML technologies.
+            </li>
+          </ul>
         </div>
         <div className="hirepage-feature hirepage-feature-3">
-
+          <h2>WHY CHOOSE US?</h2>
+          <p>for more details</p>
+          <ul>
+            <li>
+              We specialize in creating websites that are not only visually appealing but also tailored to meet the unique needs of your business, driving engagement and conversions.
+            </li>
+            <li>
+              Through our internship programs and online training platforms, we provide the next generation of tech talent with the skills
+            </li>
+            <li>
+              Knowledge needed to succeed in the ever-evolving tech industry.
+            </li>
+            <li>
+              Stay ahead of threats with our AI and ML-based threat intelligence tool, designed to offer proactive and comprehensive protection in the digital space.
+            </li>
+          </ul>
         </div>
         <div className="hirepage-feature hirepage-feature-4">
-
+          <h2>LET US TOGETHER BUILD A FLOURISHING BUSINESS</h2>
+          <p>To be successful, you have to have your heart in your business</p>
+          <ul>
+            <li>
+              When connected with us, you aren’t growing your business alone. We have your back and put in our best to contribute to the growth of your entire team and organization.
+            </li>
+            <li>
+              So, if you are looking for the right agency that’ll help you build a good online presence and bring in more conversions and revenue, we are right here!
+            </li>
+          </ul>
         </div>
         <div className="hirepage-feature hirepage-feature-5">
-
+          <h2>HIGHLY MOTIVATED TEAM WITH INNOVATIVE IDEAS</h2>
         </div>
       </div>
     </section>
