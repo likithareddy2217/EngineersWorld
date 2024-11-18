68cd734a31b4401c44e71be5a6d9afa95acf3c67
Muli surya Narendra reddy <133997432+Narendra-017@users.noreply.github.com>
2024-11-18 23:44:11 +0530

    Update navigation.js

--- src/components/navigationbar/navigation.js
+++ src/components/navigationbar/navigation.js
@@ -3,6 +3,7 @@ import "./navigation.css";
 
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -12,26 +13,29 @@ function Header() {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className={isScrolled ? 'scrolled' : ''}>
       <div className='logo-div'>
         <img src={`${process.env.PUBLIC_URL}/images/EWlogo.jpg`} alt="Logo" height={40} width={40} />
         <h1>ENGINEER'S WORLD</h1>
       </div>
-      
 
-      <nav className='nav'>
+      <button className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+
+      <nav className={`nav ${isMenuOpen ? 'open' : ''}`}>
         <div className='links'>
           <a href="#homepage">Home</a>
           <a href="#aboutpage">About Us</a>
           <a href="#hirepage">Hire Us</a>
           <a href='#contactpage'>Contact Us</a>
         </div>
-       
       </nav>
-
-
-
     </header>
   );
 }
